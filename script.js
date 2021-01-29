0b5f1fae957701eeb746e9394191f5f3de3c4241
tugowar80 <78186576+tugowar80@users.noreply.github.com>
2021-01-28 20:46:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Make a reference to the elements in the update budget section
+let incomeInput = document.querySelector("#income_input");
+let updateBudgetButton = document.querySelector("#income_input");
+
+// Create a variable that stores the value of the income input
+
+
+let updateBudgetButton = document.querySelector("#update_budget");
+console.log(updateBudgetButton)
+function updateBudget(event) {
+event.preventDefault() ;
+    console.log("Clicked!");
+}
+
+updateBudgetButton.onclick = updateBudget;
